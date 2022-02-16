5e86d41c503675ba49bc392c6d299a861f2c6e60
erabhay1993@gmail.com <erabhay1993@gmail.com>
2022-02-16 21:24:51 +0000

    Configuration of source done

--- models/staging/stripe/stg_payments.sql
+++
@@ -1,9 +0,0 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-    amount / 100 as amount, --dollars Conversion
-    created as created_at
-
-from raw.stripe.payment
